c88236cf7748f3cecdf259ac25913d240ab225d1
dev-manuel <manuel_krofficial@online.de>
2022-06-13 01:46:50 +0200

    Initial commit

---
+++ src/test/java/de/tudresden/inf/st/mathgrassserver/MathgrassServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.tudresden.inf.st.mathgrassserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathgrassServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
