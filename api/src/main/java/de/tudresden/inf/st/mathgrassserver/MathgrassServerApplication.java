27a7c516ab1ce6851c546f43ba8059f94d0bca9f
Andreas Domanowski <andreas@domanowski.net>
2022-10-12 10:39:04 +0200

    dev: Add CORS-Support

--- api/src/main/java/de/tudresden/inf/st/mathgrassserver/MathgrassServerApplication.java
+++ api/src/main/java/de/tudresden/inf/st/mathgrassserver/MathgrassServerApplication.java
@@ -4,6 +4,13 @@ import de.tudresden.inf.st.mathgrassserver.evaluator.MessageBrokerConn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -24,10 +31,10 @@ public class MathgrassServerApplication {
 
 		} catch (Exception e) {
 			System.err.println("No message queue available. Aborting...");
-/*
+
 			// TODO - shutdown application more gracefully
 			System.exit(0);
-*/
+
 		}
 
 
@@ -39,4 +46,19 @@ public class MathgrassServerApplication {
 		return new InternalResourceViewResolver();
 	}
 
+	// TODO - do not use WebSecurityConfigureAdapter, switch to org.springframework.security.web.SecurityFilterChain
+	@EnableWebSecurity
+	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.cors();
+		}
+
+		@Bean
+		CorsConfigurationSource corsConfigurationSource() {
+			UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+			source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+			return source;
+		}
+	}
 }
