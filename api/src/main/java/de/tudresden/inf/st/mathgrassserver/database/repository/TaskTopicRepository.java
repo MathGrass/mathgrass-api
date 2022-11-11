03f16d5ce473de914245bb63a36009baf546130c
tcq1 <chu.trung@web.de>
2022-11-11 13:00:38 +0100

    style: Add missing Repository annotations

--- api/src/main/java/de/tudresden/inf/st/mathgrassserver/database/repository/TaskTopicRepository.java
+++ api/src/main/java/de/tudresden/inf/st/mathgrassserver/database/repository/TaskTopicRepository.java
@@ -2,6 +2,8 @@ package de.tudresden.inf.st.mathgrassserver.database.repository;
 
 import de.tudresden.inf.st.mathgrassserver.database.entity.TaskTopicEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskTopicRepository extends JpaRepository<TaskTopicEntity,Long> {
 }
