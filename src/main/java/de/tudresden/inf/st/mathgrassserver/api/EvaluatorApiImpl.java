671b73a8472ec53472e61bcab4ed453e5a4be0ad
Andreas Domanowski <andreas@domanowski.net>
2023-01-17 13:38:17 +0100

    dev: Make static assessment endpoint return json

--- src/main/java/de/tudresden/inf/st/mathgrassserver/api/EvaluatorApiImpl.java
+++ src/main/java/de/tudresden/inf/st/mathgrassserver/api/EvaluatorApiImpl.java
@@ -6,18 +6,22 @@ import de.tudresden.inf.st.mathgrassserver.database.entity.TaskResultEntity;
 import de.tudresden.inf.st.mathgrassserver.database.repository.TaskRepository;
 import de.tudresden.inf.st.mathgrassserver.database.repository.TaskResultRepository;
 import de.tudresden.inf.st.mathgrassserver.evaluator.TaskManager;
+import de.tudresden.inf.st.mathgrassserver.model.RunStaticAssessment200Response;
+import de.tudresden.inf.st.mathgrassserver.model.RunStaticAssessmentRequest;
 import de.tudresden.inf.st.mathgrassserver.model.TaskResult;
 import de.tudresden.inf.st.mathgrassserver.transform.TaskResultTransformer;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.async.DeferredResult;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.concurrent.ExecutorService;
@@ -46,7 +50,7 @@ public class EvaluatorApiImpl extends AbstractApiElement implements EvaluatorApi
     /**
      * Constructor.
      *
-     * @param taskRepository task repository
+     * @param taskRepository       task repository
      * @param taskResultRepository task result repository
      */
     public EvaluatorApiImpl(TaskRepository taskRepository, TaskResultRepository taskResultRepository) {
@@ -111,34 +115,17 @@ public class EvaluatorApiImpl extends AbstractApiElement implements EvaluatorApi
         return output;
     }
 
-    // TODO: include in OpenAPI-Spec - datatype + request/response
-
-    /**
-     * Record containing the answer given by the user.
-     *
-     * @param answer user specified answer
-     */
-    record UserAnswer(String answer){}
-
-    /**
-     * Get the assessment for a task ID and a {@link UserAnswer}.
-     *
-     * @param taskId ID of task
-     * @param answer answer of user
-     * @return {@link ResponseEntity} containing a boolean determining the correctness of the answer
-     */
-    @PostMapping(value = "/evaluator/staticEvaluation/{taskId}", consumes = {"application/json"},
-            produces = {"application/json"})
-    public ResponseEntity<Boolean> getStaticAssessment(
-            @ApiParam(value = "ID of task", required = true) @PathVariable("taskId") Long taskId,
-            @ApiParam(value = "Answer of student", required = true) @RequestBody UserAnswer answer) {
-        // load task from repository
+    @Override
+    public ResponseEntity<RunStaticAssessment200Response> runStaticAssessment(
+            @Parameter(name = "taskId", description = "ID of task", required = true) @PathVariable("taskId") Long taskId,
+            @Parameter(name = "RunStaticAssessmentRequest", description = "Submitted answer", required = true) @Valid @RequestBody RunStaticAssessmentRequest runStaticAssessmentRequest
+    ) {
         Optional<TaskEntity> optTask = taskRepository.findById(taskId);
         if (optTask.isPresent()) {
             // compare answers
             String expectedAnswer = optTask.get().getAnswer();
-
-            return ok(expectedAnswer.equals(answer.answer()));
+            boolean result = expectedAnswer.equals(runStaticAssessmentRequest.getAnswer());
+            return ok(new RunStaticAssessment200Response().isAssessmentCorrect(result));
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
