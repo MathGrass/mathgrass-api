32353ddea538998bc0cddab0e3d1e56562f444d1
tcq1 <chu.trung@web.de>
2023-03-14 22:31:23 +0100

    style: Remove unnecessary imports

--- src/main/java/de/tudresden/inf/st/mathgrass/api/MathgrassServerApplication.java
+++ src/main/java/de/tudresden/inf/st/mathgrass/api/MathgrassServerApplication.java
@@ -6,16 +6,8 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableWebMvc
