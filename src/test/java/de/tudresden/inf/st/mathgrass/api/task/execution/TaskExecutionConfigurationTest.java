1838de8720fe00a0363271166ba29ad1a715bb80
tcq1 <chu.trung@web.de>
2023-03-31 12:45:35 +0200

    test: Improve TaskExecutionConfigurationTest

--- src/test/java/de/tudresden/inf/st/mathgrass/api/task/execution/TaskExecutionConfigurationTest.java
+++ src/test/java/de/tudresden/inf/st/mathgrass/api/task/execution/TaskExecutionConfigurationTest.java
@@ -2,6 +2,7 @@ package de.tudresden.inf.st.mathgrass.api.task.execution;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.test.context.ActiveProfiles;
@@ -10,8 +11,7 @@ import org.springframework.test.context.TestPropertySource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
- * Test class for {@link TaskExecutionConfiguration}. Tests that properties for the ThreadPoolTaskExecutor bean are
- * loaded correctly from application.properties.
+ * Test class for {@link TaskExecutionConfiguration}.
  */
 @SpringBootTest
 @ActiveProfiles(profiles = "test")
@@ -27,10 +27,31 @@ class TaskExecutionConfigurationTest {
     @Autowired
     private ThreadPoolTaskExecutor threadPoolTaskExecutor;
 
+    /**
+     * Minimal number of threads kept alive in the thread pool.
+     */
+    @Value("${taskExecutor.corePoolSize}")
+    private int corePoolSize;
+
+    /**
+     * Max number of concurrently running threads.
+     */
+    @Value("${taskExecutor.maxPoolSize}")
+    private int maxPoolSize;
+
+    /**
+     * Max number of tasks stored in the queue.
+     */
+    @Value("${taskExecutor.queueCapacity}")
+    private int queueCapacity;
+
+    /**
+     * Tests that properties for the ThreadPoolTaskExecutor bean are loaded correctly from application.properties.
+     */
     @Test
     void testApplicationPropertiesLoaded() {
-        assertEquals(1, threadPoolTaskExecutor.getCorePoolSize());
-        assertEquals(5, threadPoolTaskExecutor.getMaxPoolSize());
-        assertEquals(10, threadPoolTaskExecutor.getThreadPoolExecutor().getQueue().remainingCapacity());
+        assertEquals(corePoolSize, threadPoolTaskExecutor.getCorePoolSize());
+        assertEquals(maxPoolSize, threadPoolTaskExecutor.getMaxPoolSize());
+        assertEquals(queueCapacity, threadPoolTaskExecutor.getThreadPoolExecutor().getQueue().remainingCapacity());
     }
 }
